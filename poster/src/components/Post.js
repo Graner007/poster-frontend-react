92481aa979f7000c13eb576acafeaf4c8c65f91e
Graner007 <graner.bence007@gmail.com>
2021-05-04 10:07:55 +0200

    add images and video to posts

--- poster/src/components/Post.js
+++ poster/src/components/Post.js
@@ -8,11 +8,20 @@ const Post = ({ post }) => {
 
     const person = people.find((person) => person.id === post.personId);
 
+    let media = null;
+
+    if (post.hasImage || post.hasVideo) {
+        media = medias.filter(media => media.postId === post.id);
+    }
+
     return (
         <div className="post" key={ post.id }>
+            <div className="profile-picture"><img src={ person.profileImageRoute } alt="profile-image"/></div>
             <div className="profile-name">{ person.username }</div>
             <div className="post-date">{ post.postDate }</div>
             <div className="post-message">{ post.message }</div>
+            { post.hasImage ? media.map(image => <img src={ image.mediaRoute } />) : null }
+            { post.hasVideo ? media.map(video => <video width="400px" controls><source src={ video.mediaRoute } /></video>) : null }
             <div className="adomCount">{ post.adomCount }</div>
             <div className="commentCount">{ post.commentCount }</div>
             <div className="shareCount">{ post.shareCount }</div>
