87b6bfffb0b8ead85c2680eb1984f4caa7842436
Graner007 <graner.bence007@gmail.com>
2021-05-05 07:47:38 +0200

    feat: add right sidebar and search bar

---
+++ poster/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+const SearchBar = () => {
+    return (
+        <div className="search-bar">
+            <i class="fa fa-search"></i><input type="text" placeholder="Search Poster" style={{ fontSize: "20px" }}/>
+        </div>
+    )
+}
+
+export default SearchBar;
