b9427784e91c3fd6c63a8a9df3bd3bfe23660373
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-05-05 13:35:07 +0200

    feat: responsive navbar

--- poster/src/App.js
+++ poster/src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./components/Navbar";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import useWindowSize from "./util/useWindowSize";
+import BottomNavbar from "./components/BottomNavbar";
+
 function App() {
+  const [width, height] = useWindowSize();
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        {width > 500 && <Navbar />}
+        {width <= 500 && <BottomNavbar />}
+      </div>
+    </Router>
   );
 }
 
