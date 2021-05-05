8b276a136f98de36a705021570a7aea66d12b60b
Graner007 <graner.bence007@gmail.com>
2021-05-05 07:45:40 +0200

    feat: add HomeHeader component

--- poster/src/App.js
+++ poster/src/App.js
@@ -3,14 +3,18 @@ import PersonContextProvider from "./contexts/PersonContext";
 import PostContextProvider from "./contexts/PostContext";
 import MediaContextProvider from "./contexts/MediaContext";
 import AddPost from "./components/AddPost";
+import HomeHeader from "./components/HomeHeader";
+import SpaceAfterAddPost from "./components/SpaceAfterAddPost";
 
 function App() {
   return (
     <div className="container">
+      <HomeHeader />
       <PostContextProvider>
         <PersonContextProvider>
           <MediaContextProvider>
             <AddPost />
+            <SpaceAfterAddPost />
             <PostList />
           </MediaContextProvider>
         </PersonContextProvider>
