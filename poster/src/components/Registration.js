29054dc51d44dc8114779b311a4ba4df0ac3f22b
Graner007 <graner.bence007@gmail.com>
2021-05-18 10:57:44 +0200

    handle register error and use useState for inputs

--- poster/src/components/Registration.js
+++ poster/src/components/Registration.js
@@ -1,28 +1,46 @@
 import axios from "axios";
 import { useState } from "react";
+import { Redirect } from 'react-router-dom';
 
 const Registration = () => {
 
-    const [userName, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [birthday, setBirthday] = useState();
+    const [birthDate, setBirthDate] = useState();
 
-    const handleClick = () => {
-        const data = {
-            userName,
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [redirect, setRedirect] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const person = {
+            username,
             email,
             password,
-            birthday
+            birthDate
         }
 
-        axios.post("/registration", data)
-        .then(res => console.log(res))
+        axios.post("/registration", person)
+        .then(res => {
+            switch(res.data) {
+                case "success":
+                    setError(false);
+                    setErrorMessage("");
+                    setRedirect(true);
+                    break;
+                default:
+                    setErrorMessage(res.data);
+                    setError(true);
+            }
+        })
         .catch(err => console.log(err));
     }
 
     return (
-        <div className="registraion-field">
+        <form className="registraion-field" onSubmit={ handleSubmit }>
             <label htmlFor="username">Username:</label><br />
             <input type="text" placeholder="username..." name="username" id="username" required onChange={(e) => setUsername(e.target.value)} /><br />
             <label htmlFor="email">Email:</label><br />
@@ -30,9 +48,11 @@ const Registration = () => {
             <label htmlFor="password">Password:</label><br />
             <input type="password" placeholder="password..." name="password" id="password" required onChange={(e) => setPassword(e.target.value)} /><br />
             <label htmlFor="birthday">Birthday:</label><br />
-            <input type="date" name="birthday" id="birthday" required onChange={(e) => setBirthday(e.target.value)} /><br />
-            <input type="submit" className="button" value="Registration" onClick={ handleClick } />
-        </div>
+            <input type="date" name="birthday" id="birthday" required onChange={(e) => setBirthDate(e.target.value)} /><br />
+            <input type="submit" className="button" value="Registration" />
+            { error && <div style={{ color: 'red' }}>{ errorMessage }</div> }
+            { redirect && <Redirect to="/" /> }
+        </form>
     )
 }
 
