1fb1ed4eea2c1db881dcaa92beb8f90a5700571d
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-05-20 10:49:52 +0200

    add: ImageContainer three image handling

---
+++ poster/src/components/ImageContainer.js
@@ -0,0 +1,31 @@
+const ImageContainer = ({ images }) => {
+  const imageCount = images.length;
+  return (
+    <div className="post-image-container">
+      {imageCount <= 2 && (
+        <div className="single-image-container">
+          {images.map((image) => (
+            <img src={image.mediaRoute} />
+          ))}
+        </div>
+      )}
+      {imageCount == 3 && (
+        <div className="double-image-container">
+          <div className="double-image-first">
+            <img src={images[0].mediaRoute} />
+          </div>
+          <div className="double-image-second">
+            <div className="double-image-top-right">
+              <img src={images[1].mediaRoute} />
+            </div>
+            <div className="double-image-bottom-right">
+              <img src={images[2].mediaRoute} />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ImageContainer;
