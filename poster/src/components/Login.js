775a1e0233db61bfb91a4e642a0e6c277922f117
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-01 16:06:28 +0200

    add: wrong credentials handling

--- poster/src/components/Login.js
+++ poster/src/components/Login.js
@@ -1,27 +1,71 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [wrongCredentials, setWrongCredentials] = useState(false);
 
-    const sendLogin = (e) => {
-        e.preventDefault();
+  const sendLogin = (e) => {
+    e.preventDefault();
 
-        const person = new FormData();
-        person.append("name", name);
-        person.append("password", password);
-    }
+    const person = {
+      username: name,
+      password: password,
+    };
 
-    return (
-        <form className="login-field" onSubmit={ sendLogin }>
-            <h1 className="login-title">Log in to Poster</h1>
-            <input type="text" className="input" placeholder="Email or Username" name="name" id="name" required onChange={(e) => setName(e.target.value)} /><br />
-            <input type="password" className="input" placeholder="Password" name="password" id="password" required onChange={(e) => setPassword(e.target.value)} /><br />
-            <input type="submit" value="Log in" className="button" /><br />
-            <Link className="login-sign-up-link" to="/registration">Sign up for Poster</Link>
-        </form>
-    )
-}
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios
+      .post("/auth/login", person, config)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((data) => {
+        if (data.response.status == 403) {
+          setWrongCredentials(true);
+        }
+      });
+  };
+
+  return (
+    <form className="login-field" onSubmit={sendLogin}>
+      <h1 className="login-title">Log in to Poster</h1>
+      <input
+        type="text"
+        className="input"
+        placeholder="Email or Username"
+        name="name"
+        id="name"
+        required
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <input
+        type="password"
+        className="input"
+        placeholder="Password"
+        name="password"
+        id="password"
+        required
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br />
+      <input type="submit" value="Log in" className="button" />
+      <br />
+      <Link className="login-sign-up-link" to="/registration">
+        Sign up for Poster
+      </Link>
+      {wrongCredentials && (
+        <div className="login-error-credentials">Wrong credentials!</div>
+      )}
+    </form>
+  );
+};
 
 export default Login;
