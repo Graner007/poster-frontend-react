ff3583ff46c0df1b6dad6175b4d8adbc2a1ed20b
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-03 17:06:33 +0200

    fix: who to follow profile pictures

--- poster/src/components/ProfileCard.js
+++ poster/src/components/ProfileCard.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 
 const ProfileCard = ({ person }) => {
-    return (
-        <div className="profile-card" key={ person.id }>
-            <Link to={ '/profile/' + person.id } className="profile-link">
-                <img src={ person.profileImageId ? person.profileImageId : "/media/default-image" } alt="" className="who-to-follow-profile-image" />
-                <div className="who-to-follow-username" >{ person.username }</div>
-            </Link>
-            <button className="button follow-button">Follow</button>
-        </div>
-    )
-}
+  const imageUrl = "http://localhost:8080/media/";
+
+  return (
+    <div className="profile-card" key={person.id}>
+      <Link to={"/profile/" + person.id} className="profile-link">
+        <img
+          src={
+            person.profileImageId != null
+              ? imageUrl + person.profileImageId
+              : imageUrl + "default-image"
+          }
+          alt=""
+          className="who-to-follow-profile-image"
+        />
+        <div className="who-to-follow-username">{person.username}</div>
+      </Link>
+      <button className="button follow-button">Follow</button>
+    </div>
+  );
+};
 
 export default ProfileCard;
