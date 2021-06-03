01395f174d73779b52bd11954a325851d2af26ad
Graner007 <graner.bence007@gmail.com>
2021-06-03 11:32:07 +0200

    load follow sidebar people from backend

--- poster/src/components/FollowSidebar.js
+++ poster/src/components/FollowSidebar.js
@@ -1,10 +1,16 @@
 import ProfileCard from "./ProfileCard";
-import { useContext } from "react";
-import { PersonContext } from "../contexts/PersonContext";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const FollowSidebar = () => {
-    const { people } = useContext(PersonContext);
+    const [people, setPeople] = useState([]);
 
+    useEffect(() => {
+        axios.get("/follow-sidebar")
+            .then(res => setPeople(res.data))
+            .catch(err => console.log(err));
+    });
+ 
     return (
         <div className='who-to-follow'>
             <h2 className="who-to-follow-title">Who To Follow</h2>
